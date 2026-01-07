08c38262d7a84eb4ecb1662d70605c086416d582
Mryagci <1erkinyagci@gmail.com>
2026-01-07 09:39:45 +0300

    Folder 19-24_poses 80-100 Updated

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -447,7 +447,7 @@ export default async function BlogPostPage({ params }: PageProps) {
                   href="/poses"
                   className="px-8 py-3 bg-white/10 text-white font-semibold rounded-xl hover:bg-white/20 transition-colors"
                 >
-                  Explore 345+ Poses
+                  Explore 365+ Poses
                 </Link>
               </div>
             </Container>
